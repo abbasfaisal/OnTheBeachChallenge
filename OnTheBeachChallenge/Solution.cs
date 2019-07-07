1034824adcb1c6e26e6d602786a477e0f4d856cd
Faisal Abbas <faisal.abbas@ibexglobal.com>
2019-07-07 17:16:02 +0500

    - complete unit and console tests - add comments and documentation

--- OnTheBeachChallenge/Solution.cs
+++ OnTheBeachChallenge/Solution.cs
@@ -4,10 +4,26 @@ using System.Linq;
 
 namespace OnTheBeachChallenge
 {
+    /// <summary>
+    /// Main class that takes jobs as strings, processes them and outputs their order.
+    /// </summary>    
+    /// 
+    /// <remarks>
+    /// Job sorting is done through the algorithm known as Topological Sorting.
+    /// We pass a job, then we pass it's dependents recursively until all the dependencies and grand-dependencies of this job have been passed, saving them to the final result along the way. If a job occurs more than once during a pass, it means that input contains a circular dependency, so an argument exception is thrown.
+    /// </remarks>
     public partial class Solution
     {
+        //
+        // Summary:
+        //      Holds the sorted sequence.
+        //
         private List<char> JobSequence = new List<char>();
 
+        //
+        // Summary:
+        //      Represents a job with title and dependencies.
+        //
         private class Job<T>
             where T : struct
         {
@@ -21,6 +37,11 @@ namespace OnTheBeachChallenge
             }
         }
 
+        //
+        // Summary:
+        //      Method to parse list of strings representing jobs and converts them into jobs represented by a single character for internal processing.
+        //      Throws ArgumentException. See GetJobSequence for list of acceptable job formats.
+        //
         private List<Job<char>> ParseInput(List<string> inputJobs)
         {
             var jobs = new List<Job<char>>();
@@ -53,6 +74,11 @@ namespace OnTheBeachChallenge
             return jobs;
         }
 
+        //
+        // Summary:
+        //      Processes a list of jobs and saves result in JobSequence field.
+        //      Throws ArgumentException if input contains circular dependency.
+        //
         private void GenerateJobSequence(List<Job<char>> jobs)
         {
             this.JobSequence.Clear();
@@ -62,6 +88,10 @@ namespace OnTheBeachChallenge
                 VisitJob(job, visited);
         }
 
+        //
+        // Summary:
+        //      Helper method used by GenerateJobSequence.
+        //
         private void VisitJob(Job<char> job, List<char> visited)
         {
             if (visited.Contains(job.Title))
@@ -78,6 +108,24 @@ namespace OnTheBeachChallenge
             }
         }
 
+        /// <summary>
+        /// Public interface of the solution class.
+        /// </summary>
+        /// 
+        /// <param name="inputs">
+        /// List of strings where each string represents a job. Each job must be represented by a single character. A job and it's dependency must be separated by '=>'. Each job may have multiple dependecies but each string can contain one job and one dependency.
+        /// </param>
+        /// 
+        /// <returns>
+        /// String representing job names in ordered manner.
+        /// </returns>
+        /// 
+        /// <example>
+        /// Some examples of valid inputs are: 'a', 'a => a', 'a=>', 'a=>'.
+        /// Some examples of invalid inputs are: '=>', 'aa =>', 'a=>aaa', '', 'a=>a=>b'.
+        /// </example>
+        /// 
+        // TODO: Put this into an abstract class to work with different types of input titles. Right now this only works with jobs represented by a single character.
         public string GetJobSequence(List<string> inputs)
         {
             var jobs = ParseInput(inputs);
@@ -91,6 +139,29 @@ namespace OnTheBeachChallenge
 
         static void Main(string[] args)
         {
+            try
+            {
+                var solution = new Solution();
+                List<string>[] inputs = {
+                       new List<string>() { "a=>" },
+                       new List<string>() { "a => ", "b=>", "c=>" },
+                       new List<string>() { "a =>", "b => c", "c=>"},
+                       new List<string>() { "a=>", "b=>c", "c=>f", "d => a", "e =>b", "f=>" },
+                       new List<string>() { "a =>", "b", "c=>c" },
+                       new List<string>() { "a=>", "b=>c", "c=>f", "d=>a", "e=>", "f=>b"},
+                        };
+                Array.ForEach(inputs, input =>
+                {
+                    try { OutputToConsole(input, solution.GetJobSequence(input)); }
+                    catch (Exception ex) { OutputToConsole(input, ex.Message); }
+                });
+            }
+            finally { Console.Read(); }
+        }
+
+        private static void OutputToConsole(List<string> input, string output)
+        {
+            Console.WriteLine($"Input: {string.Join(", ", input)}. Output: {output}");
         }
     }
 }
--- OnTheBeachChallenge/Tests/Solution.cs
+++ OnTheBeachChallenge/Tests/Solution.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
 using NUnit.Framework;
 
 namespace OnTheBeachChallenge
 {
     public partial class Solution
     {
+        /// <summary>
+        /// Contains unit tests for methods of class solution. Uses NUnit.
+        /// </summary>
         [TestFixture]
         public class TestClass
         {
             private Solution solution = new Solution();
 
+            /// <summary>
+            /// Tests ParseInput and sees that valid input strings are converted to jobs correctly. 
+            /// </summary> 
             [Test]
             public void UT_ParseInput_Basic()
             {
@@ -80,6 +89,79 @@ namespace OnTheBeachChallenge
                 TestJobsList(actualJobsList, parsedJobsList);
             }
 
+            /// <summary>
+            /// Tests ParsedInput and sees that dependencies are parsed and assigned correctly.
+            /// </summary>
+            [Test]
+            public void UT_ParseInput_Dependencies()
+            {
+                var jobsList = new List<char> { 'a', 'b' };
+                var dependenciesList = new List<List<char>> { new List<char>(), new List<char>() { 'b' } };
+                var parsedJobsList = this.solution.ParseInput(new List<string>() { "a =>", "b => b" });
+
+                TestDependenciesList(jobsList, dependenciesList, parsedJobsList);
+
+                jobsList = new List<char> { 'a', 'b', 'c', 'd' };
+                dependenciesList = new List<List<char>> {
+                                                new List<char>() { 'a' },
+                                                new List<char>() { 'b', 'a' },
+                                                new List<char>(),
+                                                new List<char>() { 'd' },
+                                            };
+                parsedJobsList = this.solution.ParseInput(new List<string>() { "a => a", "b => b", "b => a", "c", "d => d", "d => " });
+
+                TestDependenciesList(jobsList, dependenciesList, parsedJobsList);
+            }
+
+            /// <summary>
+            /// Tests ParseInput() and sees that exception is thrown on receiving incorrectly formatted job string.
+            /// </summary>
+            [Test]
+            public void UT_ParseInput_InvalidFormat()
+            {
+                string titleException = "Each job must be represented by a single character"; ;
+                string formatException = "Invalid Format of Jobs";
+                TestInvalidInputFormats(new List<string>() { "=>" }, titleException);
+                TestInvalidInputFormats(new List<string>() { "=> a" }, titleException);
+                TestInvalidInputFormats(new List<string>() { "=> abc" }, titleException);
+                TestInvalidInputFormats(new List<string>() { "abc" }, titleException);
+                TestInvalidInputFormats(new List<string>() { "abc => a" }, titleException);
+                TestInvalidInputFormats(new List<string>() { "abc=>abc" }, titleException);
+                TestInvalidInputFormats(new List<string>() { "a => a => " }, formatException);
+                TestInvalidInputFormats(new List<string>() { "a => a => abc" }, formatException);
+                TestInvalidInputFormats(new List<string>() { "a => a => a" }, formatException);
+            }
+
+            /// <summary>
+            /// Basic tests for output from GetJobSequence().
+            /// </summary>
+            [Test]
+            public void UT_GetJobSequence_Basic()
+            {
+                TestJobSequence(new List<string>() { "a" }, "a");
+                TestJobSequence(new List<string>() { "a", "b", "c" }, "abc");
+                TestJobSequence(new List<string>() { "a => ", "b => c", "c =>" }, "acb");
+                var result = this.solution.GetJobSequence(new List<string>() { "a", "b => c", "c => f", "d => a", "e => b", "f" });
+                Assert.IsTrue(result.Length == 6 &&
+                              IsBefore(result, 'c', 'b') &&
+                              IsBefore(result, 'f', 'c') &&
+                              IsBefore(result, 'a', 'd') &&
+                              IsBefore(result, 'b', 'e') &&
+                              result.Contains('a'));
+            }
+
+            /// <summary>
+            /// Basic tests for output from GetJobSequence().
+            /// </summary>
+            public void UT_GetJobSequence_circularInput()
+            {
+                TestcircularDependency(new List<string>() { "a => a" });
+                TestcircularDependency(new List<string>() { "a => ", "b => ", "c => c" });
+                TestcircularDependency(new List<string>() { "a =>", "b =>c", "c => f", "d => a", " e =>", "f=>b" });
+            }
+
+            #region HelperFunctions
+
             private void TestJobsList(List<char> actualJobsList, List<Job<char>> parsedJobsList)
             {
                 Console.WriteLine("Checking number of jobs returned....");
@@ -88,6 +170,45 @@ namespace OnTheBeachChallenge
                 for (var i = 0; i < actualJobsList.Count; i++)
                     Assert.IsTrue(actualJobsList[i] == parsedJobsList[i].Title);
             }
+
+            private void TestDependenciesList(List<char> jobsList, List<List<char>> dependenciesList, List<Job<char>> parsedJobsList)
+            {
+                Assert.AreEqual(jobsList.Count, parsedJobsList.Count);
+                for (var i = 0; i < jobsList.Count; i++)
+                    Assert.AreEqual(jobsList[i], parsedJobsList[i].Title);
+
+                for (var i = 0; i < dependenciesList.Count; i++)
+                {
+                    Assert.AreEqual(dependenciesList[i].Count, parsedJobsList[i].Dependencies.Count);
+                    for (var j = 0; j < dependenciesList[i].Count; j++)
+                        Assert.IsTrue(dependenciesList[i][j] == parsedJobsList[i].Dependencies[j].Title, "");
+                }
+            }
+
+            private void TestInvalidInputFormats(List<string> inputs, string exceptionMessage)
+            {
+                var result = Assert.Throws<ArgumentException>(() => this.solution.ParseInput(inputs));
+                Assert.IsTrue(result.Message == exceptionMessage);
+            }
+
+            private void TestJobSequence(List<string> inputs, string output)
+            {
+                var result = this.solution.GetJobSequence(inputs);
+                Assert.IsTrue(result == output);
+            }
+
+            private bool IsBefore(string str, char first, char second)
+            {
+                return str.Contains(first) && str.Contains(second) && str.IndexOf(first) < str.IndexOf(second);
+            }
+
+            private void TestcircularDependency(List<string> inputs)
+            {
+                var result = Assert.Throws<ArgumentException>(() => this.solution.GetJobSequence(inputs));
+                Assert.IsTrue(result.Message == "Input contains circular dependency");
+            }
+
+            #endregion
         }
     }
 }
